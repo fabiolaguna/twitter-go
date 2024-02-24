bcf56fecc70d9be089f054826f15b9351b01995d
Fabio Laguna <fabiolaguna.94@gmail.com>
2024-02-24 16:03:20 -0300

    fix

--- services/image.go
+++ services/image.go
@@ -56,8 +56,7 @@ func UploadImage(ctx context.Context, uploadType string, request events.APIGatew
 	if strings.HasPrefix(mediaType, "multipart/") {
 		input := request.Body
 		b64data := input[strings.IndexByte(input, ',')+1:]
-		fmt.Println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA: " + b64data)
-		body, err := base64.StdEncoding.DecodeString(b64data)
+		body, err := base64.RawStdEncoding.DecodeString(b64data)
 		if err != nil {
 			response.Message = "[image service][method:UploadImage] Error decoding string: " + err.Error()
 			fmt.Println(response.Message)
